90161ce2c8448b87bb6508a8e1697d83d19d9ce7
Iago Bozza <iagobozza@writeonlycode.io>
2023-02-28 12:35:42 +0000

    Fix error

--- main.js
+++ main.js
@@ -2,10 +2,6 @@ import './style.css'
 import javascriptLogo from './javascript.svg'
 import { setupCounter } from './counter.js'
 
-function github_access_token() {
-  http://localhost:5174/?code=83552ab21a579eeabcd0
-}
-
 document.querySelector('#app').innerHTML = `
   <div>
     <h1>Hello Vite!</h1>
