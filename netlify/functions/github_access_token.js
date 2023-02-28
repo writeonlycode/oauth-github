4a7dcbe0fca527037b0a209dc1010848c0b3b69d
Iago Bozza <iagobozza@writeonlycode.io>
2023-02-28 12:43:46 +0000

    Update Function

--- netlify/functions/github_access_token.js
+++ netlify/functions/github_access_token.js
@@ -1,9 +1,6 @@
-exports.handler = (event, context) => {
+exports.handler = async function (event, context) {
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      event,
-      context,
-    }),
+    body: JSON.stringify({ message: "Hello World" }),
   };
 };
