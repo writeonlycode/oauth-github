51b9b98d7108b6cfdcd012278505530b624d0319
Iago Bozza <iagobozza@writeonlycode.io>
2023-02-28 13:25:01 +0000

    Updates

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
 
-document.querySelector('#app').innerHTML = `
+const searchParams = new URLSearchParams(window.location.search);
+const githubCode = searchParams.get("code");
+
+document.querySelector("#app").innerHTML = `
   <div>
     <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
     <p class="read-the-docs">
       Click on the Vite logo to learn more
+      <div>
+        Code: ${ githubCode }
+      </div>
+
+      <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
     </p>
   </div>
-`
-
-setupCounter(document.querySelector('#counter'))
+`;
--- netlify/functions/github_access_token.js
+++ netlify/functions/github_access_token.js
@@ -1,4 +1,29 @@
+import axios from "axios";
+
 exports.handler = async function (event, context) {
+  const code = event.queryStringParameters.code;
+  const client_id = process.env.CLIENT_ID;
+  const client_secret = process.env.CLIENT_SECRET;
+
+  if (code) {
+    const response = await axios.post(
+      `https://github.com/login/oauth/access_token`,
+      null,
+      {
+        params: {
+          code,
+          client_id,
+          client_secret,
+        },
+      }
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ response }),
+    };
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ event, context }),
