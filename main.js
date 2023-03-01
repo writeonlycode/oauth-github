30b48344c2397ed3c117a24b4e4f6c3552397f07
Iago Bozza <iagobozza@writeonlycode.io>
2023-03-01 10:52:35 +0000

    tweaks

--- main.js
+++ main.js
@@ -18,17 +18,48 @@ const start = async () => {
     return;
   }
 
+  if (code) {
+    const response = await axios.get(
+      "https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token",
+      {
+        headers: { Accept: "application/json" },
+        responseType: "json",
+        params: {
+          code,
+        },
+      }
+    );
+
+    const githubAccessToken = response.data.access_token;
+
+    if (!githubAccessToken) {
+      document.querySelector("#app").innerHTML = `
+        <div>
+          <h1>OAuth with GitHub</h1>
+          <div class="read-the-docs">
+            <p>Ops... Something went wrong while authenticating...</p>
+            <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
+          </div>
+        </div>
+      `;
+
+      return;
+    }
+
+    localStorage.setItem("github_access_token", githubAccessToken);
+  }
+
   if (localStorage.getItem("githubAccessToken")) {
-    const user = axios.get("https://api.github.com/user", {
-      Accept: "application/vnd.github+json",
-      Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
-      "X-GitHub-Api-Version": "2022-11-28",
-    });
+    // const user = axios.get("https://api.github.com/user", {
+    //   Accept: "application/vnd.github+json",
+    //   Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
+    //   "X-GitHub-Api-Version": "2022-11-28",
+    // });
 
     document.querySelector("#app").innerHTML = `
       <div>
         <h1>OAuth with GitHub</h1>
-        <p>Hi ${user.name}!</p>
+        <p>Hi!</p>
         <div class="read-the-docs">
           <div>
             <div>
@@ -44,35 +75,6 @@ const start = async () => {
 
     return;
   }
-
-  const response = await axios.get(
-    "https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token",
-    {
-      headers: { Accept: "application/json" },
-      responseType: "json",
-      params: {
-        code,
-      },
-    }
-  );
-
-  const githubAccessToken = response.data.access_token;
-
-  if (!githubAccessToken) {
-    document.querySelector("#app").innerHTML = `
-      <div>
-        <h1>OAuth with GitHub</h1>
-        <div class="read-the-docs">
-          <p>Ops... Something went wrong while authenticating...</p>
-          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
-        </div>
-      </div>
-    `;
-
-    return;
-  }
-
-  localStorage.setItem("github_access_token", githubAccessToken);
 };
 
 start();
