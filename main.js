7a066020183ba96ae9f80c1d78ddb14f61fabc75
Iago Bozza <iagobozza@writeonlycode.io>
2023-03-01 10:30:48 +0000

    Login flow

--- main.js
+++ main.js
@@ -1,53 +1,79 @@
 import "./style.css";
-import javascriptLogo from "./javascript.svg";
 import axios from "axios";
+import { Octokit } from "octokit";
 
 const start = async () => {
   const searchParams = new URLSearchParams(window.location.search);
   const code = searchParams.get("code");
-  let access_token;
 
-  if (code) {
-    const response = await axios.get(
-      "https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token",
-      {
-        headers: {'Accept': 'application/json'},
-        responseType: "json",
-        params: {
-          code,
-        },
-      }
-    );
-
-    access_token = response.data.access_token;
-
+  if (!code && !localStorage.getItem("githubAccessToken")) {
     document.querySelector("#app").innerHTML = `
       <div>
-        <h1>Hello Vite!</h1>
-        <p class="read-the-docs">
-          Click on the Vite logo to learn more
-          <div>
-            Code: ${code}
-            Access Token: ${access_token}
-          </div>
-          <pre>
-            ${JSON.stringify(response)}
-          </pre>
-
-        </p>
-      </div>
-    `;
-  } else {
-    document.querySelector("#app").innerHTML = `
-      <div>
-        <h1>Hello Vite!</h1>
-        <p class="read-the-docs">
-          Click on the Vite logo to learn more
+        <h1>OAuth with GitHub</h1>
+        <div class="read-the-docs">
           <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
-        </p>
+        </div>
       </div>
     `;
+
+    return;
   }
+
+  if (localStorage.getItem("githubAccessToken")) {
+    const octokit = new Octokit({ auth: githubAccessToken });
+
+    const {
+      data: { login },
+    } = await octokit.request("GET /user");
+
+    document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>OAuth with GitHub</h1>
+        <p>Hi ${login}!</p>
+        <div class="read-the-docs">
+          <div>
+            <div>
+              <h2>Repositories:</h2>
+              <ul>
+                <li>Repository #1</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+
+    return;
+  }
+
+  const response = await axios.get(
+    "https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token",
+    {
+      headers: { Accept: "application/json" },
+      responseType: "json",
+      params: {
+        code,
+      },
+    }
+  );
+
+  const githubAccessToken = response.data.access_token;
+
+  if (!githubAccessToken) {
+    document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>OAuth with GitHub</h1>
+        <div class="read-the-docs">
+          <p>Ops... Something went wrong while authenticating...</p>
+          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
+        </div>
+      </div>
+    `;
+
+    return;
+  }
+
+  localStorage.setItem("github_access_token", githubAccessToken);
 };
 
 start();
