4c0d7baa116be59d1b427012194c6bf8c48e6857
Iago Bozza <iagobozza@writeonlycode.io>
2023-03-01 05:45:55 +0000

    remove top level await

--- main.js
+++ main.js
@@ -2,39 +2,43 @@ import "./style.css";
 import javascriptLogo from "./javascript.svg";
 import axios from "axios";
 
-const searchParams = new URLSearchParams(window.location.search);
-const code = searchParams.get("code");
+const start = async () => {
+  const searchParams = new URLSearchParams(window.location.search);
+  const code = searchParams.get("code");
 
-if (code) {
-  const response = await axios.get("https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token/", null, {
-    params: {
-      code,
-    },
-  });
+  if (code) {
+    const response = await axios.get("https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token/", null, {
+      params: {
+        code,
+      },
+    });
 
-  console.log(response);
+    console.log(response);
 
-  document.querySelector("#app").innerHTML = `
-    <div>
-      <h1>Hello Vite!</h1>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-        <pre>
-          ${JSON.stringify(response)}
-        </pre>
+    document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>Hello Vite!</h1>
+        <p class="read-the-docs">
+          Click on the Vite logo to learn more
+          <pre>
+            ${JSON.stringify(response)}
+          </pre>
 
-      </p>
-    </div>
-  `;
+        </p>
+      </div>
+    `;
 
-} else {
-  document.querySelector("#app").innerHTML = `
-    <div>
-      <h1>Hello Vite!</h1>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-        <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
-      </p>
-    </div>
-  `;
-}
+  } else {
+    document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>Hello Vite!</h1>
+        <p class="read-the-docs">
+          Click on the Vite logo to learn more
+          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
+        </p>
+      </div>
+    `;
+  }
+};
+
+start();
