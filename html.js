8f30f1b90ba9c8a35c13687f3a38039a67d10761
Iago Bozza <iagobozza@writeonlycode.io>
2023-03-02 06:53:04 +0000

    Updates

---
+++ html.js
@@ -0,0 +1,53 @@
+export function loginPage() {
+  document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>OAuth with GitHub</h1>
+        <div>
+          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46&scope=read:repo_hook">Log In with GitHub</a>
+        </div>
+      </div>
+    `;
+}
+
+export function errorPage() {
+  document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>OAuth with GitHub</h1>
+        <div class="read-the-docs">
+          <p>Ops... Something went wrong while authenticating...</p>
+          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
+        </div>
+      </div>
+    `;
+}
+
+export async function loggedPage() {
+    const user = await axios.get("https://api.github.com/user", {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
+      },
+    });
+
+    const repos = await axios.get("https://api.github.com/user/repos", {
+      headers: {
+        Accept: "application/vnd.github+json",
+        Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
+      },
+    });
+
+    document.querySelector("#app").innerHTML = `
+      <div>
+        <h1>OAuth with GitHub</h1>
+        <p>Hi ${user.data.name}!</p>
+        <div>
+          <div>
+            <div>
+              <h2>Repositories:</h2>
+              ${ repos.data.map((e) => `<p>${e.name}</p>`).join(" ") }
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+}
--- main.js
+++ main.js
@@ -1,24 +1,30 @@
 import "./style.css";
 import axios from "axios";
+import {errorPage, loggedPage, loginPage} from "./html";
 
 const start = async () => {
   const searchParams = new URLSearchParams(window.location.search);
   const code = searchParams.get("code");
 
   if (!code && !localStorage.getItem("githubAccessToken")) {
-    document.querySelector("#app").innerHTML = `
-      <div>
-        <h1>OAuth with GitHub</h1>
-        <div class="read-the-docs">
-          <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46&scope=read:repo_hook">Log In with GitHub</a>
-        </div>
-      </div>
-    `;
+    loginPage();
+    return;
+  }
 
+  if (localStorage.getItem("githubAccessToken")) {
+    loggedPage();
     return;
   }
 
   if (code) {
+    searchParams.delete("code");
+
+    if (searchParams.toString() !== "") {
+      history.replaceState({}, "", "?" + searchParams.toString());
+    } else {
+      history.replaceState({}, "", "/");
+    }
+
     const response = await axios.get(
       "https://gleeful-sundae-16db0f.netlify.app/.netlify/functions/github_access_token",
       {
@@ -30,61 +36,15 @@ const start = async () => {
       }
     );
 
-    console.log(response);
-
     const githubAccessToken = response.data.access_token;
 
     if (!githubAccessToken) {
-      document.querySelector("#app").innerHTML = `
-        <div>
-          <h1>OAuth with GitHub</h1>
-          <div class="read-the-docs">
-            <p>Ops... Something went wrong while authenticating...</p>
-            <a class="button" href="https://github.com/login/oauth/authorize?client_id=9b5cefd5346e0dc66d46">Log In with GitHub</a>
-          </div>
-        </div>
-      `;
-
+      errorPage();
       return;
     }
 
     localStorage.setItem("githubAccessToken", githubAccessToken);
-  }
-
-  if (localStorage.getItem("githubAccessToken")) {
-    const user = await axios.get("https://api.github.com/user", {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
-      },
-    });
-
-    const repos = await axios.get("https://api.github.com/user/repos", {
-      headers: {
-        Accept: "application/vnd.github+json",
-        Authorization: `Bearer ${localStorage.getItem("githubAccessToken")}`,
-      },
-    });
-
-    console.log(user);
-    console.log(repos);
-
-    document.querySelector("#app").innerHTML = `
-      <div>
-        <h1>OAuth with GitHub</h1>
-        <p>Hi ${user.data.name}!</p>
-        <div>
-          <div>
-            <div>
-              <h2>Repositories:</h2>
-              ${ repos.data.map((e) => `<p>${e.name}</p>`).join(" ") }
-            </div>
-          </div>
-        </div>
-      </div>
-    `;
-
-    return;
+    loggedPage();
   }
 };
 
